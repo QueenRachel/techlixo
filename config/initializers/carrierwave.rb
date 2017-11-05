0f399c51eb3574b8dbb1b88f1f1e9e3a31ccc38e
Unknown <raquelbrandao007@hotmail.com>
2017-11-05 18:48:40 -0200

    Alterada região de funcionamento da Amazon AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,12 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    region:                'us-west-2',
+    region:                'us-east-1',
     provider:              'AWS',
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'] || '',
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || '',
   }
   config.storage = (Rails.env.production? || Rails.env.staging?) ? :fog : :file
-  config.store_dir = "#{Rails.env}/uploads"
   config.fog_directory = ENV['AWS_BUCKET_NAME'] || ''
   config.enable_processing = false if Rails.env.test?
 end

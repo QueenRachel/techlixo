00c924f6d0633590c5190fb8cd054c6931bd65b3
Unknown <raquelbrandao007@hotmail.com>
2017-11-05 18:24:41 -0200

    Arrumado problema com carrierwave no heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,4 @@
 CarrierWave.configure do |config|
-  config.storage = (Rails.env.production? || Rails.env.staging?) ? :fog : :file
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     region:                'us-west-2',
@@ -7,6 +6,7 @@ CarrierWave.configure do |config|
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'] || '',
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || '',
   }
+  config.storage = (Rails.env.production? || Rails.env.staging?) ? :fog : :file
   config.store_dir = "#{Rails.env}/uploads"
   config.fog_directory = ENV['AWS_BUCKET_NAME'] || ''
   config.enable_processing = false if Rails.env.test?

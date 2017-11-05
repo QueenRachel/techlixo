0c0eaad9d867422539ea2952e947203c49037952
Unknown <raquelbrandao007@hotmail.com>
2017-11-05 18:51:37 -0200

    Adicionado store_photo! para que seja enviada a imagem a AWS

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,7 @@ class ContactsController < ApplicationController
     # Esta linha checa se todas as valicações são verdadeiras
     # Nada é salvo em banco, portanto não usar o .save
     if @contact.valid?
+      @contact.store_photo!
       destino = Business.find(contact_params[:business_id]).email
       # Envia o email de contato
       ContactMailer.contact(@contact, destino).deliver_now
